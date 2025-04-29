6c4b911f3623cc696d8e264208ded2a1c063fd8e
taikula <125849709+taikulayyds@users.noreply.github.com>
2025-04-29 22:42:10 +0800

    Update GitHub429.js

--- GitHub429.js
+++ GitHub429.js
@@ -1,9 +1,5 @@
 #!name=github429
 #!desc=解决github更新资源429问题
-#!openUrl=https://axs66.github.io/repo
-#!author=A先生[https://qq.ios999.vip/axs/Axs.jpg]
-#!homepage=https://axs66.github.io/repo
-#!icon=http://q4.qlogo.cn/headimg_dl?dst_uin=1317208960&spec=640
 #!date=2025-04-29
 
 [Rewrite]
